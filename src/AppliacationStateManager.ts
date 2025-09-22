00fa407e97eeb7350d1c93fd9e660423d2bb0128
phasenull <55253035+phasenull@users.noreply.github.com>
2025-09-22 18:09:47 +0300

    payload hashing stuff

--- src/AppliacationStateManager.ts
+++ src/AppliacationStateManager.ts
@@ -9,8 +9,40 @@ interface IWarning {
 export abstract class ApplicationStateManager {
 	static warnings: IWarning[] = []
 	private static status: "healthy" | "partial" | "fatal" = "healthy"
+	private static config: any = {}
 	public static GetStatus() {
 		return this.status
+	}
+	public static GetConfig() {
+		return this.config
+	}
+	public static SetConfig(config: any) {
+		this.config = config
+		Logger.log(`Set application config to ${JSON.stringify(this.config,undefined,4)}`,"ApplicationStateManager")
+	}
+	public static async signPayload(payload: any) {
+		if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined")
+		const secret = process.env.JWT_SECRET
+		const stringified = JSON.stringify(payload)
+		const encoded = Buffer.from(stringified).toString("base64")
+		const hasher = new Bun.CryptoHasher("sha256",secret)
+		hasher.update(encoded)
+		const signature = hasher.digest("hex")
+		return `${encoded}.${signature}`
+	}
+	public static async verifyPayload(signed_payload: string) {
+		if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined")
+		const secret = process.env.JWT_SECRET
+		
+		const [encoded, signature] = signed_payload.split(".")
+		const hasher = new Bun.CryptoHasher("sha256",secret)
+		hasher.update(encoded)
+		const expected_signature = hasher.digest("hex")
+		const is_equal = crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(expected_signature))
+		if (!is_equal) throw new Error("Invalid signature")
+		const stringified = Buffer.from(encoded, "base64").toString("utf-8")
+		const payload = JSON.parse(stringified)
+		return payload
 	}
 	public static SetStatus(warning: IWarning) {
 		this.warnings.push(warning)
